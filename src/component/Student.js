dc91ba1461d44d705139efd0bf23c918e8dac353
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2020-12-17 16:14:36 -0800

    the test grade of each student is replaced with progress bar

--- src/component/Student.js
+++ src/component/Student.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import '../component/Student.css';
-import { Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
 
 function Student(props){
   const [open, setOpen] = useState(false);
@@ -19,7 +20,12 @@ function Student(props){
   const gradeList = grades.map(grade=>{
    return (
       <div className='grade' key={Math.random()}>
-        Test{grades.indexOf(grade)+1}: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{grade}%
+        Test{grades.indexOf(grade)+1}: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        <div id="myProgress">
+          <div id="myBar" style={{width:`${grade}%`}}>{grade}%</div>
+        </div>
+        
       </div>
      )
   })
@@ -41,18 +47,19 @@ function Student(props){
         <img src={props.student.pic} alt={props.student.firstName}></img>
       </div>
       <div className='info'>
-      <h1 className='name'>{props.student.firstName.toUpperCase()} {props.student.lastName.toUpperCase()}</h1>
-      <div className='email'>Email: {props.student.email}</div>
-      <div className='company'>Company: {props.student.company}</div>
-      <div className='skill'>Skill: {props.student.skill}</div>
-      <div className='average'>Average: {props.median}%</div>
+        <h1 className='name'>{props.student.firstName.toUpperCase()} {props.student.lastName.toUpperCase()}</h1>
+        <div className='email'>Email: {props.student.email}</div>
+        <div className='company'>Company: {props.student.company}</div>
+        <div className='skill'>Skill: {props.student.skill}</div>
+        <div className='average'>Average: {props.median}%</div>
       
-      {open && <div className='all-grades' id="example-collapse-text">
+        {open && 
+          <div className='all-grades' id="example-collapse-text">
             {gradeList}
             <div className='all-tags'>
               {tagList}
             </div>
-          <input className='add-tag-input' placeholder='Add a tag' onKeyDown={onKeyDown}></input>
+            <input className='add-tag-input' placeholder='Add a tag' onKeyDown={onKeyDown}></input>
           </div>}
      
       </div>
