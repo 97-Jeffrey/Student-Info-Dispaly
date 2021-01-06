717f7877d3dfc658e52c270f9bfd93aaa22a84d9
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-01-05 20:58:06 -0800

    the matchName function is changed

--- src/helper/matchName.js
+++ src/helper/matchName.js
@@ -3,7 +3,9 @@ const matchName = function(person, name){
 
   const lastNameFilter = person.lastName.toLowerCase().includes(name.toLowerCase());
   const firstNameFilter = person.firstName.toLowerCase().includes(name.toLowerCase());
-  return lastNameFilter || firstNameFilter? true:false;
+  const fullName = `${person.firstName} ${person.lastName}`;
+  const fullNameFilter = fullName.toLowerCase().includes(name.toLowerCase());
+  return lastNameFilter || firstNameFilter || fullNameFilter? true:false;
 
 }
 module.exports = matchName;
